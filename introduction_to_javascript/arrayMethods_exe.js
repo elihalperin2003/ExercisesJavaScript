9a3d88588fca547222443d78077533665f36b00e
eli <eelihalperin@gmail.com>
2026-06-23 11:58:32 +0300

    arrayMethods - exe 4

--- introduction_to_javascript/arrayMethods_exe.js
+++ introduction_to_javascript/arrayMethods_exe.js
@@ -5,3 +5,9 @@ let arr2 = ["hello", "world"];
 arr2 = arr2.map((word) => word.toUpperCase());
 
 // console.log(arr2);
+
+// 4
+
+const arr4 = [1, 2, 3, 4, 5, 6];
+
+// console.log(arr4.filter((num) => !(num % 2)));
