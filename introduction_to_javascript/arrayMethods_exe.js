75e4965a19c13d10d781c639dc174a08682eebfb
eli <eelihalperin@gmail.com>
2026-06-23 12:08:36 +0300

    arrayMethods - exe 8

--- introduction_to_javascript/arrayMethods_exe.js
+++ introduction_to_javascript/arrayMethods_exe.js
@@ -11,3 +11,13 @@ arr2 = arr2.map((word) => word.toUpperCase());
 const arr4 = [1, 2, 3, 4, 5, 6];
 
 // console.log(arr4.filter((num) => !(num % 2)));
+
+// 8
+
+const prices = [111, 222, 99, 88, 100];
+
+const filterAndMap = prices
+  .filter((price) => price > 100)
+  .map((price) => Math.round(price * 1.1));
+
+console.log(filterAndMap);
