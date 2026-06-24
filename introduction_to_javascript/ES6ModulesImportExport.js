3b3a37295376f2b4e505425b5bc86eeb098919f7
eli <eelihalperin@gmail.com>
2026-06-24 12:15:41 +0300

    ES6ModulesImportExport - exe

---
+++ introduction_to_javascript/ES6ModulesImportExport.js
@@ -0,0 +1,50 @@
+// 1
+
+import { sum } from "./exportsES6Modules/exports.js";
+
+// console.log(sum(9, 2));
+
+// 2
+
+import { greet } from "./exportsES6Modules/exports.js";
+
+// console.log(greet("abu"));
+
+// 3
+
+import { pi } from "./exportsES6Modules/exports.js";
+
+// console.log(pi);
+
+// 4
+
+// import momo from "./exportsES6Modules/exports.js";
+
+// console.log(momo(5));
+
+// 5
+
+import { greet2 } from "./exportsES6Modules/exports.js";
+
+// 6
+
+// import obj from "./exportsES6Modules/exports.js";
+
+// console.log(obj);
+
+// 7
+
+// לא למדנו מחלקות
+
+// 8
+
+import obj, { name } from "./exportsES6Modules/exports.js";
+
+// console.log(obj);
+// console.log(name);
+
+// 9
+
+import { c } from "./exportsES6Modules/exports.js";
+
+console.log(c(1, 6));
---
+++ introduction_to_javascript/exportsES6Modules/exports.js
@@ -0,0 +1,19 @@
+export const sum = (a, b) => a + b;
+
+export const greet = (name) => `hello ${name}`;
+
+export const greet2 = (name) => `hello ${name}`;
+
+export const pi = 3.14152;
+
+export const name = "Loopy Abu Mishel";
+
+// export default function mult(num) {
+//   return num * 2;
+// }
+
+export default { name: "Mishel" };
+
+import { add } from "./exports2.js";
+
+export const c = add;
