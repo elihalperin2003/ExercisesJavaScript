9f7da0812e94a37b5c2a6db14be2e0e04b700521
eli <eelihalperin@gmail.com>
2026-07-08 12:26:45 +0300

    creating function - POST createuser

--- servers/express/server.js
+++ servers/express/server.js
@@ -5,16 +5,24 @@ const dir = await fs.readdir(process.cwd());
 
 if (!dir.includes("users.json")) await fs.writeFile("users.json", "[]");
 
-// // reading function
-// async function getUsers() {
-//   const data = await fs.readFile("users.json");
-//   const users = JSON.parse(data);
-//   return users;
-// }
-// // writing function
-// async function writeUsers(users) {
-//   const data = await fs.writeFile("users.json", JSON.stringify(users, null, 2));
-// }
+// reading function
+async function getUsers() {
+  const data = await fs.readFile("users.json");
+  const users = JSON.parse(data);
+  return users;
+}
+
+// writing function
+async function writeUsers(users) {
+  const data = await fs.writeFile("users.json", JSON.stringify(users, null, 2));
+}
+
+// validation
+function validation(res, name, password) {
+  if (!name || !password) {
+    res.status(400).json({ message: "must be name and password" });
+  }
+}
 
 const server = express();
 
@@ -39,9 +47,16 @@ server.get("/api/names/:name", (req, res) => {
 });
 
 // post
-// server.post("api/users", (req, res) => {
-//   const { name, password } = req.body;
-// });
+server.post("/api/users", async (req, res) => {
+  const { name, password } = req.body;
+  validation(res, name, password);
+  const users = await getUsers();
+  const id = Math.max(0, ...users.map((user) => user.id)) + 1;
+  const user = { id, name, password };
+  users.push(user);
+  await writeUsers(users);
+  res.status(201).json({ message: "User created!" });
+});
 
 // listening
 server.listen(3000, () => {
